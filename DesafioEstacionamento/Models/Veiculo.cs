0aa40ee343d2ced335e4ecdd68ce7259b5bac6b5
Davi Bittencourt <dbittencourt2002@gmail.com>
2023-12-07 14:51:46 -0300

    feat: implementação da classe veículo

--- DesafioEstacionamento/Models/Veiculo.cs
+++ DesafioEstacionamento/Models/Veiculo.cs
@@ -1,12 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DesafioEstacionamento.Models
 {
     public class Veiculo
     {
-        // A implementar
+        private string placa;
+        private string modelo;
+        private string cor;
+
+        public Veiculo(string placa, string modelo, string cor)
+        {
+            if (placaValida(placa))
+            {
+                this.placa = placa;
+                this.modelo = modelo;
+                this.cor = cor;
+            }
+            else
+            {
+                throw new ArgumentException($"A placa do carro é inválida: {placa}\nDeve ser seguido o padrão de Placa Mercosul. Exemplo: ABC-1D23");
+            } 
+        }
+
+        private bool placaValida(string placa)
+        {
+            string padraoPlaca = @"^[A-Z]{3}-\d{1}[A-Z]{1}\d{2}$";
+
+            return Regex.IsMatch(placa, padraoPlaca);
+        }
+
+        public override string ToString()
+        {
+            return $"Placa: {placa}\nModelo: {modelo}\nCor: {cor}";
+        }
     }
 }
