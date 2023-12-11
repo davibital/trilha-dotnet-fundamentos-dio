fd06d1354587fb202e59af53a1a9f3e4c50b952a
Davi Bittencourt <dbittencourt2002@gmail.com>
2023-12-11 11:06:39 -0300

    feat: método para adicionar um veículo ao estacionamento

--- DesafioEstacionamento/Models/Estacionamento.cs
+++ DesafioEstacionamento/Models/Estacionamento.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,7 +20,27 @@ namespace DesafioEstacionamento.Models
 
         public void AdicionarVeiculo()
         {
+            Console.WriteLine("Informe o modelo do veículo: ");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Informe a cor do veículo: ");
+            string cor = Console.ReadLine();
+            Console.WriteLine("Informe a placa do veículo: ");
+            string placa = Console.ReadLine();
 
+            try
+            {
+                veiculos.Add(new Veiculo(placa, modelo, cor));
+            } 
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("[ERRO]");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERRO]");
+                Console.WriteLine("Ocorreu um erro inesperado: " + e.Message);
+            }
         }
 
         public void RemoverVeiculo()
