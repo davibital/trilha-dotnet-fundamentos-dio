3b6f02c061fc7bbe6d4c3a1bf2535346ee55bd8b
Davi Bittencourt <dbittencourt2002@gmail.com>
2023-12-11 11:04:59 -0300

    feat: execução do sistema de estacionamento

--- DesafioEstacionamento/Program.cs
+++ DesafioEstacionamento/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesafioEstacionamento.Models;
+
+decimal precoInicial;
+decimal precoPorHora;
+
+Console.WriteLine("Seja bem-vindo ao sistema de estacionamento");
+
+Console.WriteLine("Digite o preço inicial: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço por hora: ");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+bool sistemaEmExecucao = true;
+
+while (sistemaEmExecucao)
+{
+  Console.Clear();
+  Console.WriteLine("Digite a sua opção: ");
+  Console.WriteLine("1 - Cadastrar veículo");
+  Console.WriteLine("2 - Remover veículo");
+  Console.WriteLine("3 - Listar veículos");
+  Console.WriteLine("4 - Encerrar");
+
+  switch (Console.ReadLine())
+  {
+    case "1":
+      estacionamento.AdicionarVeiculo();
+      break;
+    case "2":
+      estacionamento.RemoverVeiculo();
+      break;
+    case "3":
+      estacionamento.ListarVeiculos();
+      break;
+    case "4":
+      sistemaEmExecucao = false;
+      Console.WriteLine("Sistema encerrando...");
+      break;
+    default:
+      Console.WriteLine("Opção inválida!");
+      break;
+  }
+
+  Console.WriteLine("Pressione uma tecla para continuar.");
+  Console.ReadLine();
+}
